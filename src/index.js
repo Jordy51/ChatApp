5a346adf7db14e92fceebbd2752d3aaefa683aba
Jordy <adityakumawat51@gmail.com>
2020-12-08 17:31:44 +0530

    Socket.IO introduced

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-const express = require("express");
 const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app);
+const io = socketio(server);
 
+const PORT = 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.json());
 app.use(express.static(publicDirectoryPath));
 
-app.get("/", () => {
-	app.render("index.html");
+// let count = 0;
+
+io.on("connection", (socket) => {
+	console.log("New WebSocket connection");
+	
+	socket.emit("message", "Welcome!");
+
+	socket.on("sendMessage", (message) => {
+		io.emit("message", message);
+	});
+
+	// socket.emit("countUpdated", count);
+
+	// socket.on("increment", () => {
+	// 	count++;
+	// 	// socket.emit("countUpdated", count);
+	// 	io.emit("countUpdated", count);
+	// });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log("Server is up on port " + PORT);
 });
